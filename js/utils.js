8c511aab7117acf4c8c2237a2db644c493f1cf00
M Auf <mmfauf@gmail.com>
2026-03-02 10:05:31 +0300

    fix: escapeHtml coerces value to String before calling .replace

--- js/utils.js
+++ js/utils.js
@@ -101,7 +101,7 @@ export function avatarColor(name = '') {
  * @returns {string}
  */
 export function escapeHtml(str = '') {
-  return str
+  return String(str)
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
     .replace(/>/g, '&gt;')
